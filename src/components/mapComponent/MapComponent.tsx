5f303b4917e24259b01bbd536210d7675e5b87cf
fmg-hariom <se003212@funmaxgames.com>
2024-10-26 18:36:54 +0530

    create contact us  page

--- src/components/mapComponent/MapComponent.tsx
+++ src/components/mapComponent/MapComponent.tsx
@@ -23,7 +23,7 @@ const MapComponent = () => {
       center={[latitude, longitude]}
       zoom={13}
       ref={mapRef}
-      style={{ height: "100%", width: "100%", borderRadius: "10px" }} 
+      style={{ height: "100%", width: "100%", borderRadius: "10px" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -32,7 +32,7 @@ const MapComponent = () => {
       <Marker position={[latitude, longitude]} icon={customIcon}>
         <Popup>
           <div style={{ textAlign: "center" }}>
-            <div style={{ fontSize: "30px" }}>Fun max Games</div>
+            <div style={{ fontSize: "32px" }}>Fun max Games</div>
             <b style={{ fontSize: "30px" }}>A-63, Vidhyut Nagar</b>
             <br />
             <span style={{ fontSize: "30px" }}>Jaipur, Rajasthan 302021</span>
